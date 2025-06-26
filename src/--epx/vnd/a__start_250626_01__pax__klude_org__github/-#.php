99dd779847b2b627223c3db2db9ee65a021de982
klude-au <124860775+klude-au@users.noreply.github.com>
2025-06-26 20:14:15 +0930

    update: Including the first plugin

---
+++ src/--epx/vnd/a__start_250626_01__pax__klude_org__github/-#.php
@@ -0,0 +1,11 @@
+<?php
+
+class a__start_250626_01__pax__klude_org__github {
+    
+    public static function _() { static $i;  return $i ?: ($i = new static()); }
+    
+    public function __invoke(){
+        echo __METHOD__.":".__FILE__;
+    }
+    
+}
