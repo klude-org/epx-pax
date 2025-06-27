0d036f2aa32c790e23f9e37f87d70c7f9d01ad1f
klude-au <124860775+klude-au@users.noreply.github.com>
2025-06-27 15:18:22 +0930

    update

---
+++ src/--epx/vnd/epx__std_ui__pax__klude_org__github/v0/-#.php
@@ -0,0 +1,14 @@
+<?php namespace epx__std_ui__pax__klude_org__github;
+
+final class v0 {
+    
+    public static function _() { static $i;  return $i ?: ($i = new static()); }
+    
+    private function __construct(){
+    }
+    
+    public function __invoke(){
+        echo __METHOD__.":".__FILE__.PHP_EOL;
+    }
+    
+}
