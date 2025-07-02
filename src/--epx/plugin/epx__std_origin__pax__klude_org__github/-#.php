c7c838410c56365b09d1a0ca4ddc6459b58e77bf
klude-au <124860775+klude-au@users.noreply.github.com>
2025-07-02 09:52:04 +0930

    update

--- src/--epx/plugin/epx__std_origin__pax__klude_org__github/-#.php
+++ src/--epx/plugin/epx__std_origin__pax__klude_org__github/-#.php
@@ -193,8 +193,6 @@ final class epx__std_origin__pax__klude_org__github extends \stdClass implements
         $intfc = \_\INTFC;
         $this->cfg_cache_t = \is_file($this->cfg_cache_f = \_\LOCAL_DIR."/.config-cache-{$intfc}.php") ? \filemtime($this->cfg_cache_f) : 0;
         if($CFG_MODE || !$this->cfg_cache_t){
-            $this->ds = \_\START_DIR;
-            $this->dl = \_\INCP_DIR.'/--epx';
             $this->cfg_parts = [
                 \_\START_DIR."/.lib-config.php" => 1,
                 \_\START_DIR."/.lib-config-{$intfc}.php" => 1,
@@ -215,6 +213,8 @@ final class epx__std_origin__pax__klude_org__github extends \stdClass implements
             if($build){
                 (function(){
                     $_ = [];
+                    $this->dl = \_\INCP_DIR.'/--epx';
+                    $this->ds = \_\START_DIR;
                     $this->trace = \str_pad("# ",80,"#").PHP_EOL;
                     foreach($this->cfg_parts as $k => $v){
                         if($v && \is_file($k)){
@@ -225,22 +225,23 @@ final class epx__std_origin__pax__klude_org__github extends \stdClass implements
                     $this->_ = $_;
                     $this->_['LSP'] = \array_filter(\iterator_to_array((function(){
                         yield $this->dl => true;
-                        $this->trace .= "# Library directory included: '".\str_replace('\\','/', $this->dl)."'".PHP_EOL;
+                        $this->trace .= "# Library directory included: '{$this->dl}'".PHP_EOL;
                         foreach($this->_['LIBRARIES'] ?? [] as $dx => $en){
+                            $dx = \str_replace('\\','/', $dx);
                             if($en){
                                 if(\is_dir($dx)){
-                                    $this->trace .= "# Library directory included: '".\str_replace('\\','/', $dx)."'".PHP_EOL;
+                                    $this->trace .= "# Library directory included: '{$dx}'".PHP_EOL;
                                     yield \str_replace('\\','/', \realpath($dx)) => true;
                                 } else {
-                                    $this->trace .= "# Library directory not found: '".\str_replace('\\','/', $dx)."'".PHP_EOL;
+                                    $this->trace .= "# Library directory not found: '{$dx}'".PHP_EOL;
                                 }
                             } else {
-                                $this->trace .= "# Library directory disabled: '".\str_replace('\\','/', $dx)."'".PHP_EOL;
+                                $this->trace .= "# Library directory disabled: '{$dx}'".PHP_EOL;
                             }
                         }
                         if($this->ds != $this->dl){
                             yield $this->ds => true;
-                            $this->trace .= "# Library directory included: '".\str_replace('\\','/', $this->ds)."'".PHP_EOL;
+                            $this->trace .= "# Library directory included: '{$this->ds}'".PHP_EOL;
                         }
                     })()));
                     $this->_['TSP']['PATH'] = \implode(PATH_SEPARATOR, $this->_['TSP']['LIST'] = \array_keys(\array_filter(\iterator_to_array((function(){
@@ -314,7 +315,10 @@ final class epx__std_origin__pax__klude_org__github extends \stdClass implements
                         $contents, 
                         LOCK_EX // prevents race when testing and you have ton of simultaneous requests
                     );
-                })();
+                })->bindTo((object)[
+                   'cfg_parts' => $this->cfg_parts,
+                   'cfg_cache_f' => $this->cfg_cache_f
+                ])();
             }
         }
 
