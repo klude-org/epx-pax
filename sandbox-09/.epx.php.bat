89427ca8ad142f0608e183b9141058672126e867
klude-au <124860775+klude-au@users.noreply.github.com>
2025-07-19 20:17:40 +0930

    update

--- sandbox-09/.epx.php.bat
+++ sandbox-09/.epx.php.bat
@@ -29,7 +29,16 @@ SET FW__EPX_START_DIR=%~dp0--epx
 set FW__EPX_START_PHP=%~dp0--epx\.start.php
 if "%FX__ORIGINAL_PATH%"=="" set "FX__ORIGINAL_PATH=%Path%"
 set PATH=%FW__EPX_START_DIR%\std-shell;%FX__ORIGINAL_PATH%
-call :do_setup
+:do_setup
+rem [93mLaunching setup[0m
+::strange block behaviour stupid parser design tons of gotchas >:(
+C:/xampp/current/php__xdbg/php.exe "%~f0" || (
+    echo [94mERROR LEVEL %errorlevel%[0m
+    echo [91mError during setup[0m
+    goto :exit_error
+)
+if %errorlevel%==0 goto :setup_done
+:setup_done
 call %FW__SHELL_BAT%
 if %errorlevel% NEQ 0 goto :exit_error
 :session_defined
@@ -77,9 +86,9 @@ if %errorlevel%==0 goto :exit_ok
 :exit_error
 rem [93mRunning error handler[0m
 if %errorlevel%==2000 goto :launch_cmd
+echo [91m!!! Encountered An Error !!![0m
 echo %cmdcmdline% | findstr /i /c:" /c" >nul
 if %errorlevel%==0 pause
-echo [92m!!! Encountered An Error !!![0m
 exit /b 1
 ::------------------------------------------------------------------------------
 :launch_cmd
@@ -152,15 +161,6 @@ for /f "tokens=1 delims==" %%A in ('set FW__') do (
 )
 exit /b 0
 
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:do_setup
-rem [93mLaunching setup[0m
-C:/xampp/current/php__xdbg/php.exe "%~f0"
-if %errorlevel%==0 goto :setup_done
-echo [91mError during setup[0m
-exit /b 1
-:setup_done
-exit /b 0
 <?php endif;
 (new class extends \stdClass {
     public function __invoke(){
@@ -170,6 +170,11 @@ exit /b 0
             1 AND \ini_set('display_errors', 0);
             1 AND \ini_set('display_startup_errors', 1);
             1 AND \ini_set('error_reporting', E_ALL);
+            0 AND error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED); //To silence warnings and notices but still catch fatal errors and exceptions:
+            1 AND error_reporting(E_ERROR); //catch only fatal error
+            0 AND set_error_handler(function($errno, $errstr, $errfile, $errline) { //handle manually
+                throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+            });
             if(empty($_SERVER['FW__SITE_DIR'])){
                 throw new \Exception("Invalid Site DIR");
             }
@@ -193,27 +198,47 @@ exit /b 0
                 SET "FX__CONFIG_LOADED=1"
                 BAT);
             }
-            if(!\is_file($start_php_fpath)){
+            if(!\is_dir($lib_dpath)){
                 \is_dir($d = \dirname($start_php_fpath)) OR \mkdir($d, 0777, true);
                 $url_base = "https://raw.githubusercontent.com/klude-org/epx-pax/main/libraries/{$lib_type}";
+                echo "[93mDownloading From '{$url_base}'[0m\n";
                 if(!($contents = \file_get_contents($url = "{$url_base}/.manifest.json"))){
                     throw new \Exception("Library --epx: Failed to download manifest from '{$url}'");
                 }
                 if(!($manifest = \json_decode($contents,true))){
                     throw new \Exception("Library --epx: Failed to decode manifest from '{$url}'");
                 }
+                $failed = false;
                 foreach($manifest['files'] ?? [] as $rpath => $v){
-                    echo "[93mDownloading '{$url}'[0m";
+                    echo "[33mDownloading '{$rpath}'[0m";
                     if(!($contents = \file_get_contents($url = "{$url_base}/{$rpath}"))){
-                        echo "[91m Download Failed: '{$rpath}'[0m\n";
+                        $failed = true;
+                        echo "[91m Download Failed !!![0m\n";
                     } else {
                         \is_dir($d = \dirname($fpath = "{$lib_dpath}/{$rpath}")) OR \mkdir($d, 0777, true);
                         if(\file_put_contents($fpath, $contents) == false){
-                            echo "[91m Write Failed'{$url}'[0m\n";
+                            $failed = true;
+                            echo "[91m Write Failed !!![0m\n";
                         } else{
-                            echo "[92m Downloaded'{$url}'[0m\n";
+                            echo "[32m Downloaded[0m\n";
                         }
                     }
+                }
+                if($failed){
+                    1 AND (function($d){if(\is_dir($d)){
+                        foreach(new \RecursiveIteratorIterator(
+                            new \RecursiveDirectoryIterator($d, \RecursiveDirectoryIterator::SKIP_DOTS)
+                            , \RecursiveIteratorIterator::CHILD_FIRST
+                        ) as $f) {
+                            if ($f->isDir()){
+                                rmdir($f->getRealPath());
+                            } else {
+                                unlink($f->getRealPath());
+                            }
+                        }
+                        rmdir($d);
+                    }})($lib_dpath);
+                    throw new \Exception("Library --epx: Failed to install '{$url}'");
                 }
             }
             if(!\is_file($start_php_fpath)){
@@ -234,3 +259,5 @@ exit /b 0
         }
     }
 })();
+
+__halt_compiler();
