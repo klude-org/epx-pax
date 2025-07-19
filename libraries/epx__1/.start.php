811fd0da8a8557347e8c366804c5791b91891031
klude-au <124860775+klude-au@users.noreply.github.com>
2025-07-19 20:27:48 +0930

    update

--- libraries/epx__1/.start.php
+++ libraries/epx__1/.start.php
@@ -2,26 +2,22 @@
 #region PRE-START
 namespace {
     \defined('_\MSTART') OR \define('_\MSTART', \microtime(true));
-    global $_;
-    (isset($_) && \is_array($_)) OR $_ = [];
-    1 AND \ini_set('display_errors', 0);
-    1 AND \ini_set('display_startup_errors', 1);
-    1 AND \ini_set('error_reporting', E_ALL);
-    \defined('_\LIB_NAME_DEFAULT') OR \define('_\LIB_NAME_DEFAULT','');
-    if(($_['LIB_NAME'] ?? '') == ''){
-        return include '.start-0.php';
-    } else if(\is_file($f = "--epx".($_['LIB_NAME'] ?? '').'/.start.php')){
-        return include $f;
-    } else if(\str_replace('\\','/',__FILE__) !== ($f = (function(){
+    if(\str_replace('\\','/',__FILE__) !== ($f = (function(){
         try {
             global $_;
+            (isset($_) && \is_array($_)) OR $_ = [];
             $_['LIB_TYPE'] ??= 'epx__250718_01__lib';
-            $_['LIB_NAME'] ??= \_\LIB_NAME_DEFAULT;
+            $_['LIB_NAME'] ??= '';
             $plugin_dpath= \str_replace('\\','/', \dirname(__DIR__));
             $lib_type = $_['LIB_TYPE'];
             $lib_name = "--epx{$_['LIB_NAME']}";
             $lib_dpath = "{$plugin_dpath}/{$lib_name}";
-            $start_php_fpath = "{$lib_dpath}/.start.php";
+            $start_php_fpath = \str_replace('\\','/', "{$lib_dpath}/.start.php");
+            
+            if(\is_file($start_php_fpath)){
+                return $start_php_fpath;
+            }
+            
             if(!\is_dir($lib_dpath)){
                 if(!\is_file($start_php_fpath)){
                     $url = "https://raw.githubusercontent.com/klude-org/epx-pax/main/libraries/{$lib_type}/.start.php";
@@ -34,6 +30,7 @@ namespace {
                     }
                 }
             }
+            
             if(!\is_file($start_php_fpath)){
                 throw new \Exception("Library '{$lib_type}': Missing .start.php");
             }
