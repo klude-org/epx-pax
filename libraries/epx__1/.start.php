c0de8c5f967235e37f6dfbf82f1c274ec0b67de5
klude-au <124860775+klude-au@users.noreply.github.com>
2025-07-19 21:53:41 +0930

    update

--- libraries/epx__1/.start.php
+++ libraries/epx__1/.start.php
@@ -2,10 +2,15 @@
 #region PRE-START
 namespace {
     \defined('_\MSTART') OR \define('_\MSTART', \microtime(true));
+    \defined('_\OB_START') OR \define('_\OB_START', \ob_get_level());
     if(\str_replace('\\','/',__FILE__) !== ($f = (function(){
         try {
             global $_;
             (isset($_) && \is_array($_)) OR $_ = [];
+            1 AND \ini_set('display_errors', 0);
+            1 AND \ini_set('display_startup_errors', 1);
+            1 AND \ini_set('error_reporting', E_ALL);
+            0 AND \error_reporting(E_ALL);
             $_['LIB_TYPE'] ??= 'epx__250718_01__lib';
             $_['LIB_NAME'] ??= '';
             $plugin_dpath= \str_replace('\\','/', \dirname(__DIR__));
@@ -19,7 +24,7 @@ namespace {
             }
             
             if(!\is_dir($lib_dpath)){
-                \is_dir($d = \dirname($start_php_fpath)) OR \mkdir($d, 0777, true);
+                try {
                 $url_base = "https://raw.githubusercontent.com/klude-org/epx-pax/main/libraries/{$lib_type}";
                 if(!($contents = \file_get_contents($url = "{$url_base}/.manifest.json"))){
                     throw new \Exception("Library --epx: Failed to download manifest from '{$url}'");
@@ -39,6 +44,9 @@ namespace {
                     }
                 }
                 if($failed){
+                        throw new \Exception("Library --epx: Failed to install '{$url}'");
+                    }
+                } catch(\Exception $ex){
                     1 AND (function($d){if(\is_dir($d)){
                         foreach(new \RecursiveIteratorIterator(
                             new \RecursiveDirectoryIterator($d, \RecursiveDirectoryIterator::SKIP_DOTS)
@@ -52,7 +60,7 @@ namespace {
                         }
                         rmdir($d);
                     }})($lib_dpath);
-                    throw new \Exception("Library --epx: Failed to install '{$url}'");
+                    throw $ex;
                 }
             }
             
@@ -78,7 +86,7 @@ namespace {
                 } break;
                 case 'web':{
                     \http_response_code(500);
-                    while(\ob_get_level() > \_\OB_OUT){ @\ob_end_clean(); }
+                    while(\ob_get_level() > \_\OB_START){ @\ob_end_clean(); }
                     \defined('_\SIG_ABORT') OR \define('_\SIG_ABORT', -1);
                     echo <<<HTML
                         <pre style="overflow:auto; color:red;border:1px solid red;padding:5px;">{$ex}</pre>
@@ -87,7 +95,7 @@ namespace {
                 } break;
                 default:{
                     \http_response_code(500);
-                    while(\ob_get_level() > \_\OB_OUT){ @\ob_end_clean(); }
+                    while(\ob_get_level() > \_\OB_START){ @\ob_end_clean(); }
                     \defined('_\SIG_ABORT') OR \define('_\SIG_ABORT', -1);
                     \header('Content-Type: application/json');
                     echo \json_encode([
